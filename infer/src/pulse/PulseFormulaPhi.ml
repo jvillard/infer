274272fddb74afea3fd8b0986b8c7a03c3da5714
Jules Villard <jvillard@jvillard.net>
2026-06-04 15:00:02 +0100

    remove rogue call to propagate_atom

--- infer/src/pulse/PulseFormulaPhi.ml
+++ infer/src/pulse/PulseFormulaPhi.ml
@@ -1074,6 +1074,8 @@ module Normalizer : sig
   (** use with the result of {!normalize_atom} in place of {!and_atom} *)
 
   val and_atom : Atom.t -> t * new_eqs -> add_term:bool -> (t * new_eqs) SatUnsat.t
+  (** [and_atom atom (phi, new_eqs)] is
+      [SatUnsat.(normalize_atom phi atom >>= and_normalized_atoms (phi, new_eqs))] *)
 
   val and_dynamic_type :
     Var.t -> Typ.t -> ?source_file:SourceFile.t -> t * new_eqs -> (t * new_eqs) SatUnsat.t
@@ -1081,11 +1083,6 @@ module Normalizer : sig
   val and_below : Var.t -> Typ.t -> t * new_eqs -> (t * new_eqs) SatUnsat.t
 
   val and_notbelow : Var.t -> Typ.t -> t * new_eqs -> (t * new_eqs) SatUnsat.t
-
-  val propagate_atom : Atom.t -> t * new_eqs -> (t * new_eqs) SatUnsat.t
-
-  (** [and_atom atom (phi, new_eqs)] is
-      [SatUnsat.(normalize_atom phi atom >>= and_normalized_atoms (phi, new_eqs))] *)
 end = struct
   (* Use the monadic notations when normalizing formulas. *)
   open SatUnsat.Import
